907b6965895e4b12f85ba94fd7b5d86b687120da
CarloColletti~ <carlocolletti5@gmail.com>
2023-03-07 16:38:36 +0100

    fix

--- src/components/AppTvSeries.vue
+++ src/components/AppTvSeries.vue
@@ -45,15 +45,16 @@ export default {
       <h2 v-if="store.series.length > 0" class="text-white fs-2 py-4">
         Ho trovato: {{ store.series.length }} Serie Tv
       </h2>
-      <div class="row row-cols-4 g-5">
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-5">
         <AppCard v-for="serie in store.series" class="d-flex flex-columm" 
-        :Name="serie.original_title"
-        :Title="serie.title" 
-        :Language="serie.original_language" 
-        :Vote="serie.vote_average"
-        :BackCover="serie.backdrop_path" 
-        :FrontCover="serie.poster_path" 
-        :Overview="serie.overview" />
+          :Name="serie.original_title"
+          :Title="serie.title" 
+          :Language="serie.original_language" 
+          :Vote="serie.vote_average"
+          :BackCover="serie.backdrop_path" 
+          :FrontCover="serie.poster_path" 
+          :Overview="serie.overview" 
+        />
       </div>
     </div>
   </section>
